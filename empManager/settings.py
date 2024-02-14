328f53b8ad490558efec1711f2d8d6e75a787aee
TabishAnsari <ansaritabish007@gmail.com>
2024-02-15 03:37:36 +0530

    after changing allowed hosts

--- empManager/settings.py
+++ empManager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7qbn0zn$^1heg@br7o%2g-ii(r7%t901u+r6@k7i*0l9=^p2!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['employeemanager-mwv2.onrender.com']
 
 
 # Application definition
